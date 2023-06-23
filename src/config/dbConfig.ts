cabfbbff6e2ddb82005ebee696bb7fe13db2af49
beuleu <billyklotz789@gmail.com>
2023-06-23 11:16:50 +0200

    fix mongoDB connection

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-export const main = async () => {
-  await mongoose.connect('mongodb://localhost:27017/cDisPLON');
+export const main = async (mongoURI:string) => {
+  await mongoose.connect(mongoURI);
   console.log("Connected to MongoDB");
 }
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { main }    from "./config/dbConfig";
 import * as dotenv from "dotenv";
 dotenv.config()
 
-main().catch(err => console.log(err));
+main(process.env.MONGO_URI).catch(err => console.log(err));
 
 const port = process.env.PORT
 app.listen(port, () => console.log(`connecter au stage sur le port ${ port }`))
