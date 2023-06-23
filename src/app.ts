68aa263d232fce5d7c11f27b9163db6e42a17dfa
beuleu <billyklotz789@gmail.com>
2023-06-23 09:10:14 +0200

    move database start in index.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,7 @@
 import express from 'express';
-import * as dotenv from "dotenv"
-import { main }    from "./config/dbConfig";
 
-dotenv.config()
 const app = express()
 
-main().catch(err => console.log(err));
 app.use(express.json());
 
 export default app
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import app from "./app";
+import { main }    from "./config/dbConfig";
+import * as dotenv from "dotenv";
+dotenv.config()
+
+main().catch(err => console.log(err));
 
 const port = process.env.PORT
-
 app.listen(port, () => console.log(`connecter au stage sur le port ${ port }`))
