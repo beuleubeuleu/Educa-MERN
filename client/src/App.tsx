24a7ebbc01f4b2966ba45ab72490f61cd4339300
beuleu <billyklotz789@gmail.com>
2023-06-25 09:55:09 +0200

    client: BlogList component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,12 @@
 import "./App.css"
 import { Header } from "./components/Header/Header.tsx";
-import useFetch   from "./hooks/useFetch.ts";
-import { articleType } from "./types/articleType.ts";
-import { Loader } from "./components/Loader/Loader.tsx";
-import { BlogCard } from "./components/BlogCard/BlogCard.tsx";
+import { BlogList } from "./components/BlogList/BlogList.tsx";
 
 function App() {
-  const { data, error, isLoading } = useFetch<{ articles: articleType[] }>("/api/article");
-  console.log(data?.articles);
-
   return (
       <>
         <Header/>
-        {isLoading && <Loader/>}
-        {error && <p>what the heck</p>}
-        {data && data.articles.map(article=> <BlogCard article={article} key={article._id}/>)}
+        <BlogList/>
       </>
   )
 }
