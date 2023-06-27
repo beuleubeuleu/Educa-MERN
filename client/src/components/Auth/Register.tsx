190272a7182d70bb0abc19560701d3e971b83c2d
beuleu <billyklotz789@gmail.com>
2023-06-27 11:30:20 +0200

    client: authent working

--- client/src/components/Auth/Register.tsx
+++ client/src/components/Auth/Register.tsx
@@ -1,10 +1,10 @@
 import React, { useRef, useState } from "react";
 import AuthService                 from "../../services/AuthService";
-import { Link }                    from "react-router-dom";
+import { Link, useNavigate }       from "react-router-dom";
 
 const Register: React.FC = () => {
+  const navigate = useNavigate()
   const [showPassword, setShowPassword] = useState(false);
-  const [isRegisterSuccess, setIsRegisterSuccess] = useState(false);
 
   const nomRef = useRef<HTMLInputElement>(null);
   const emailRef = useRef<HTMLInputElement>(null);
@@ -15,7 +15,6 @@ const Register: React.FC = () => {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(professeurRef)
 
     try {
       await AuthService.register(
@@ -25,7 +24,7 @@ const Register: React.FC = () => {
           passwordRef.current!.value,
           professeurRef.current!.checked? professeurRef.current!.value: élèveRef.current!.value
       )
-      setIsRegisterSuccess(true)
+      navigate("/")
     } catch (error: any) {
       console.log(error)
     }
@@ -45,16 +44,17 @@ const Register: React.FC = () => {
             <input className="" type="tel" name="phone" placeholder="Prénom" required ref={ prenomRef }/>
 
             <label htmlFor="role">Vous vous inscrivez en tant que:
-              <input type="radio" id="eleve" name="role" value="élève"  ref={élèveRef} defaultChecked/>
+              <input type="radio" id="eleve" name="role" value="élève" ref={ élèveRef } defaultChecked/>
               <label htmlFor="eleve">Élève</label>
-              <input type="radio" id="professeur" name="role" value="professeur" ref={professeurRef} />
+              <input type="radio" id="professeur" name="role" value="professeur" ref={ professeurRef }/>
               <label htmlFor="professeur">Professeur</label>
             </label>
           </div>
 
           <div className="">
             <input className="" type="email" name="email" placeholder="Email" required ref={ emailRef }/>
-            <input className="" type="password" name="password" placeholder="Mot de passe" required
+            <input className="" type={ showPassword? "text": "password" } name="password" placeholder="Mot de passe"
+                   required
                    ref={ passwordRef }/>
             <label className="">
               <input className="" type="checkbox" onChange={ handleShowPassword }/>
--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,9 +1,8 @@
 import "./Header.css"
 import { Fragment, useEffect, useState } from "react";
 import { useUserContext }                from "../../context/UserContext.tsx";
-import { Link, useNavigate } from "react-router-dom";
-import CategoriesService from "../../services/CategoriesService.ts";
-import categoriesService                 from "../../services/CategoriesService.ts";
+import { Link, useNavigate }             from "react-router-dom";
+import CategoriesService                 from "../../services/CategoriesService.ts";
 
 type menuType = {
   title: string;
@@ -19,8 +18,8 @@ export const Header = () => {
 
   const getCategoriesMenuItem = async () => {
     const catégories = await CategoriesService.getAll()
-    const catégoriesMenuItem = catégories.map((catégorie: any)=>{
-      return {title: catégorie.titre, path:`/categorie/${catégorie._id}`, id:catégorie._id}
+    const catégoriesMenuItem = catégories.map((catégorie: any) => {
+      return { title: catégorie.titre, path: `/categorie/${ catégorie._id }`, id: catégorie._id }
     })
     setCatégoriesMenuItem(catégoriesMenuItem)
   }
@@ -34,28 +33,28 @@ export const Header = () => {
   const invitéMenu: menuType = [
     { title: "S'inscrire", path: "/inscription" },
     { title: "Se connecter", path: "/connexion" },
-    { title: "À propos", path: "/" },
+    { title: "À propos", path: "/" }
   ];
 
   const élèveMenu: menuType = [
     { title: "Article", path: "/article" },
     { title: "Catégorie", path: "/categorie", children: catégoriesMenuItem },
-    { title: "profile", path: "/profile" },
+    { title: "profile", path: "/profile" }
   ];
 
   const professeurMenu: menuType = [
     { title: "Article", path: "/article" },
     { title: "Catégorie", path: "/categorie", children: catégoriesMenuItem },
     { title: "Écrire un article", path: "/article/nouveau" },
-    { title: "profile", path: "/profile" },
+    { title: "profile", path: "/profile" }
   ];
 
   const adminMenu: menuType = [
     { title: "Validation-Professeur", path: "/admin/prof-application" },
-    { title: "Dashboard", path: "/admin/dashboard" },
+    { title: "Dashboard", path: "/admin/dashboard" }
   ];
 
-  if (!user) {
+  if ( !user ) {
     menu = invitéMenu
   }
 
@@ -66,9 +65,9 @@ export const Header = () => {
     case "professeur":
       menu = professeurMenu
       break;
-      case "admin":
-        menu = adminMenu
-          navigate("/admin")
+    case "admin":
+      menu = adminMenu
+      navigate("/admin")
       break;
     default:
       menu = invitéMenu
@@ -77,7 +76,7 @@ export const Header = () => {
 
   return (
       <>
-        <header className="header">
+        <header className="header" >
           <input aria-label="Mobile Menu" type="checkbox" className="header__burger"/>
 
           <Link to="/" className="header__logo">
@@ -105,7 +104,7 @@ export const Header = () => {
                   </Fragment>
               ))
             }
-                    {user&& <button onClick={()=> logout()} >Déconnexion</button>}
+            { user && <button onClick={ () => logout() }>Déconnexion</button> }
           </div>
         </header>
       </>
